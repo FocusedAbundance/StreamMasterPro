49bae3c326644df1120f9a8a723f28b527132b12
FocusedAbundance <focusedabundance2025@gmail.com>
2025-05-03 16:58:30 +0200

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+"use client"
+
+import ActivityFeed from "../ClientTrackPro/client/src/components/dashboard/ActivityFeed"
+
+export default function SyntheticV0PageForDeployment() {
+  return <ActivityFeed />
+}
